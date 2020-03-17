97087490dc6215a4507235557d87c5a0d03266a7
Cooldev <cooldev3324@gmail.com>
2020-03-18 03:10:52 +1000

    test step

--- scrapy.py
+++ scrapy.py
@@ -2,7 +2,9 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 import json
 
@@ -24,7 +26,6 @@ class ExtractMovies():
         self.login_url = 'https://www.disneyplus.com/login'
         self.browser.maximize_window()
         self.browser.get(self.login_url)
-
         self.wait_for('dssLogin')
         login_form = self.browser.find_element_by_id('dssLogin')
         login_form.find_element_by_id('email').send_keys(self.login_email)
@@ -34,14 +35,11 @@ class ExtractMovies():
         login_form.find_element_by_id('password').send_keys(self.login_pwd)
         login_form.submit()
         self.wait_for('home-collection')
-        # self.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        # self.browser.execute_script("window.scrollTo(0,0)")
         
         home_collection = self.browser.find_element_by_id('home-collection')
-        collections = home_collection.find_elements_by_xpath("./div")
+        collections = home_collection.find_elements_by_xpath('./div')
         i = 2
-        # cnt_collection = len(collections)
-        cnt_collection = 2
+        cnt_collection = len(collections)
 
         self.browser.implicitly_wait(10) # seconds
         sections = []
@@ -55,16 +53,21 @@ class ExtractMovies():
 
             slick_track = collection.find_element_by_class_name('slick-track')
             slick_next = collection.find_element_by_class_name('slick-next')
+            slick_prev = collection.find_element_by_class_name('slick-prev')
 
             while True:
-                if slick_next:
-                    try:
-                        slick_next.click()
-                    except:
-                        break
+                try:
+                    slick_next.click()
+                except:
+                    break
+            while True:
+                try:
+                    slick_prev.click()
+                except:
+                    break
 
             images = slick_track.find_elements_by_tag_name('img')
-            print(len(images))
+
             for image in images:
                 movie = {}
                 movie['Name'] = image.get_attribute('alt')
@@ -72,12 +75,69 @@ class ExtractMovies():
                 section['Items'].append(movie)
             sections.append(section)
             i = i + 1
+
+        # self.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.browser.execute_script("window.scrollTo(0,0)")
+        with open('movie.json', 'w') as json_file:
+            json.dump(sections, json_file)        
+        return sections
+    def get_movie_urls(self, sections, start_section, end_section):
+        i = start_section
+        while i < end_section:
+            if i == 2: # pass `Continue Watching`
+                i = i + 1
+                continue
+            print(sections[i]['Name'])
+            movie_cnt = len(sections[i]['Items'])
+            print(movie_cnt)
+            j = 0
+            slick_next_cnt = 0
+            while j < movie_cnt:
+                try:
+                    home_collection = self.browser.find_element_by_id('home-collection')
+                    collections = home_collection.find_elements_by_xpath('./div')
+                    collection = collections[i + 2]
+                    slick_track = collection.find_element_by_class_name('slick-track')
+                    slick_next = collection.find_element_by_class_name('slick-next')
+                    slick_prev = collection.find_element_by_class_name('slick-prev')
+                    links = collection.find_elements_by_tag_name('a')
+
+                    start = end = org_start = org_end = 0
+                    while True:
+                        while start == org_start and end == org_end:
+                            slick_divs = slick_track.find_elements_by_xpath("./div[@aria-hidden='false']")
+                            showed_movie_cnt = len(slick_divs)
+                            start = int(slick_divs[0].get_attribute('data-index'))
+                            end = int(slick_divs[showed_movie_cnt - 1].get_attribute('data-index'))
+                        print(start, end, j)
+                        if j >= start and j <= end:
+                            links[j].click()
+                            sections[i]['Items'][j]['URL'] = self.browser.current_url
+                            print(j, self.browser.current_url)
+                            self.browser.execute_script("window.history.go(-1)")
+                            break
+                        org_start = start
+                        org_end = end
+                        try:
+                            if j > end:
+                                slick_next.click()
+                            elif j < start:
+                                slick_prev.click()
+                        except:
+                            break
+                except:
+                    print('****************')
+                    self.browser.get('https://www.disneyplus.com/')
+                j = j + 1
+            with open('movie' + str(i) + '.json', 'w') as json_file:
+                json.dump(sections[i], json_file)
+            i = i + 1
         with open('movie.json', 'w') as json_file:
             json.dump(sections, json_file)
-        print(sections)
+        self.browser.quit()
 if __name__ == "__main__":
-
     ExtractMovies = ExtractMovies()
-    limit_row = ExtractMovies.get_movies()
-
+    sections = ExtractMovies.get_movies()
+    print(len(sections))
+    ExtractMovies.get_movie_urls(sections, 9, len(sections))
     print("Task completed")
